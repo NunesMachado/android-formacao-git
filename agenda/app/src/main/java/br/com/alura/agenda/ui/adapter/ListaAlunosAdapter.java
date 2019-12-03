674340470cf0b9bb93165562ed54f04788df56cc
Roberson Nunes Machado <roberson.machado@wssim.com.br>
2019-12-03 13:06:58 -0300

    Android parte 3 - Vinculando dados ao Adapter

--- agenda/app/src/main/java/br/com/alura/agenda/ui/adapter/ListaAlunosAdapter.java
+++ agenda/app/src/main/java/br/com/alura/agenda/ui/adapter/ListaAlunosAdapter.java
@@ -5,6 +5,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -41,7 +42,12 @@ public class ListaAlunosAdapter extends BaseAdapter {
         View viewCriada = LayoutInflater
                 .from(context)
                 .inflate(R.layout.item_aluno, parent, false);
-        return viewCriada;
+       Aluno alunoDevolvido = alunos.get(position);
+       TextView nome = viewCriada.findViewById(R.id.item_aluno_nome);
+       TextView telefone = viewCriada.findViewById(R.id.item_aluno_telefone);
+       nome.setText(alunoDevolvido.getNome());
+       telefone.setText(alunoDevolvido.getTelefone());
+       return viewCriada;
     }
 
     public void clear() {
