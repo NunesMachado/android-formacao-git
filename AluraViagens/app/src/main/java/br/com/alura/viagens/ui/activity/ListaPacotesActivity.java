89a97fda7048d949639dcf499183dd2ebd0eef76
Roberson Nunes Machado <roberson.machado@wssim.com.br>
2020-01-09 13:34:48 -0300

    Apredendo mexer na view

--- AluraViagens/app/src/main/java/br/com/alura/viagens/ui/activity/ListaPacotesActivity.java
+++ AluraViagens/app/src/main/java/br/com/alura/viagens/ui/activity/ListaPacotesActivity.java
@@ -8,9 +8,12 @@ import br.com.alura.viagens.R;
 
 public class ListaPacotesActivity extends AppCompatActivity {
 
+    private static final String TITULO_LISTA_PACOTES = "Pacotes";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setTitle(TITULO_LISTA_PACOTES);
         setContentView(R.layout.activity_lista_pacotes);
     }
 }
